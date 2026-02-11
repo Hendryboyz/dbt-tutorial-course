b54c8407c53f06015465e86eece5419e90977c6e
HENRY.CHOU <hendry.js1247@gmail.com>
2026-02-11 15:04:41 +0800

    feat: try to add ephemeral model

--- lessons/models/marts/dim_orders.sql
+++ lessons/models/marts/dim_orders.sql
@@ -30,6 +30,10 @@ SELECT
 	om.total_profit,
 	om.total_discount,
 
+	TIMESTAMP_DIFF(od.created_at, user_data.first_order_created_at, DAY) AS days_since_first_order
+
 FROM {{ ref("stg_ecommerce__orders") }} AS od
 LEFT JOIN order_items_measures AS om
     ON od.order_id = om.order_id
+LEFT JOIN {{ ref('int_ecommerce__first_or_created') }} AS user_data
+	ON od.user_id = user_data.user_id
